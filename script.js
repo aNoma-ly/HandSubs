a999bb0bcfc3d43aad95f93dcee25be1c2e56a5c
Dave Marais <davemarais14223@gmail.com>
2020-12-04 11:47:28 +0000

    Slide Perfection

--- script.js
+++ script.js
@@ -1,44 +1,79 @@
-var i=0;
-var images = [
-            ['assets/images/amazon_prime.jpg', 'AmazonPrime'],
-            ['assets/images/youtube_premium.png', 'Youtube'],
-            ['assets/images/spotify.png', 'Spotify'],
-            ['https://play-lh.googleusercontent.com/OuZqDwJcoCna3sbEjlV58dwBxk2bFYdgwRqe3xOphhAm5RymSSfud3qNSy4pSaRYB9M', 'END']];
-           window.onload = skipSub;
 
+// for each unactive subscription in the database add to array category
+//subData x6 and renaimed to each category
+var subData = [{"name":"Spotify", "img":"assets/images/spotify.png", "downloads":10000},
+                { "name":"Amazon", "img":"assets/images/amazon_prime.jpg", "downloads":20000},
+                { "name":"Youtube", "img":"assets/images/youtube_premium.png", "downloads":300000}]
+        
+sortArr();
 
-/*images[0][0] = 'https://m.media-amazon.com/images/I/41UqZEoLeLL._SS180_.jpg';
-images[0][1] = 'Spotify';
-images[1][0] = 'https://d1qxviojg2h5lt.cloudfront.net/images/01CQW61H9Z8SQ9Y63ENN801MAH/youtubepremium400.png';
-images [1][1] = 'Youtube'
-images[2][0] = 'https://image.roku.com/developer_channels/prod/01ff04e620e7157e570077460ad8aa9fe1a5b2699f92e00bae7f756cac890013.png';
-images [2][1] = 'Amazon'*/
-function skipSub(){
-    
-    if(i<images.length){
-        document.catOne.src = images[i][0];
-        i++;
-    }
+function sortArr(){
+    subData.sort(function(a, b){return b.downloads - a.downloads}); //sorts subscriptions by popularity
 }
+        
+var subscription = {
+                
+    name: "",
+    img : "",
+    downloads: 0
+}
+
+cardCounter = 0;
+window.onload =  skipSub;
 
 function addSub(){
     
-    if (i==0){
-        console.log('AmazonPrime');
-        i++;
+    if (subData.length == 0){
+        alert("You have reached the end of the list");     
+    }
+    else if(subData.length==1){
+        skipSub(); 
+        cardCounter--;
+       
+        subData.shift(); //array should be emty now
+        document.catOne.src = "";
     }
     else{
-        if(i<images.length){ 
-            document.catOne.src = images[i][0];
-            console.log(images[i-1][1]);  
-            i++;      
-    }
-    }
     
-
-    //setTimeout("changeImg()", time);
+    
+    var Temp = subData[0]; //swap first element in array with added suggestion
+    cardCounter--;
+  
+    subData[0] = subData[cardCounter] //prepare to remove element
+  
+    subData[cardCounter] = Temp; //return orginal first element to array
+   
+    subData.shift(); //remove added suggestion
+     
+    sortArr(); //sort array to orginal format
+    skipSub();     
+    
+    
+}
 }
 
-
-
-
+function skipSub(){
+    if(subData==0)
+    {
+        alert("You have reached the end of the list");
+        document.catOne.src = "";
+       }
+    else{
+        try{
+        loadSub(cardCounter);
+        document.catOne.src = subscription.img;     
+        cardCounter++;
+         }
+        catch{
+            cardCounter = 0;
+            skipSub();
+        }
+    }
+        
+}
+    
+function loadSub(cardCounter){
+    subscription.name = subData[cardCounter].name;
+    subscription.img =subData[cardCounter].img;
+    subscription.downloads =subData[cardCounter].downloads;  
+}
